fc4bfbc8850e674103744f11d4ea8bd47eca1a2d
Awuor Marvin <awuor.marvin5@gmail.com>
2017-06-12 20:22:51 -0700

    Added test for correctly handling 2

--- prime_generator.py
+++ prime_generator.py
@@ -1,2 +1,12 @@
 def prime_number_generator(maximum):
-    pass
+    ls = []
+    for num in range(2, maximum + 1):
+        if num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                    break
+            else:
+                print(num)
+                ls.append(num)
+
+    return ls
--- test_prime_number_generator.py
+++ test_prime_number_generator.py
@@ -4,3 +4,7 @@ from prime_generator import prime_number_generator
 class PrimeGeneratorTest(unittest.TestCase):
     def test_prime_generator_only_takes_integers(self):
         self.assertIsInstance(19, int, msg="The test failed because you passed in a non-int")
+
+    def test_check_that_2_is_not_forgotten(self):
+        result = prime_number_generator(2)
+        self.assertIn(2, result, msg="Please check how your function handles number 2")
